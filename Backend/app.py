f692c269bec417e4657b3f6f28bf19d65e79956a
Hautea <arh230003@utdallas.edu>
2025-09-16 22:19:47 -0500

    Added app.run() and modified some changes

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello world flask"
+  return "Hello World Flask"
+
+if __name__ == "__main__":
+  app.run()
