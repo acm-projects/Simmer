06bcaf57ed6cbbebbeadc6ff606dc3e9dfa44466
gocomets <adrian.hautea@gmail.com>
2025-09-20 17:02:52 -0500

    Connected Supbase to Flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, render_template, redirect, url_for
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from supabase import create_client, Client
 
 app = Flask(__name__)
 
+load_dotenv()
+supa_url: str = os.environ.get("SUPABASE_URL")
+supa_key: str = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(supabase_url=supa_url, supabase_key=supa_key)
+
 @app.route("/")
 def home():
-  return "Hello World Flask"
+  response = supabase.table('users').select('*').execute()
+  return jsonify(response.data)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(host='0.0.0.0', port=5000, debug=True)
