a6768ed13a8773fe66ce7e091b9db4d3381be91e
root <hieu.q.tran532@gmail.com>
2025-10-11 02:13:55 -0500

    added chat endpoint

--- Backend/routes/chat.py
+++ Backend/routes/chat.py
@@ -2,9 +2,11 @@ import getpass
 import os
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
-from langgraph.checkpoint.memory import MemorySaver
+# from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph import START, MessagesState, StateGraph
 from langchain.chat_models import init_chat_model
+import sqlite3
 from flask import Blueprint, request, jsonify, send_file
 from gtts import gTTS
 import io
@@ -15,6 +17,21 @@ if not os.environ.get("GOOGLE_API_KEY"):
   os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
 chat_bp = Blueprint('chat', __name__)
+model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+workflow = StateGraph(state_schema=MessagesState)
+
+def call_model(state: MessagesState):
+  response = model.invoke(state["messages"])
+  return {"messages": response}
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+conn = sqlite3.connect("conversations.db", check_same_thread=False)
+
+memory = SqliteSaver(conn=conn)
+
+app = workflow.compile(checkpointer=memory)
+
 
 @chat_bp.route("/create_chat", methods=["POST"])
 def create_chat():
@@ -23,18 +40,8 @@ def create_chat():
     return jsonify({'message': 'Missing data: recipe, cid'}), 400
   cid = data.get('cid')
   recipe = data.get('recipe')
-  workflow = StateGraph(state_schema=MessagesState)
-
-  def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
-    return {"messages": response}
-  workflow.add_edge(START, "model")
-  workflow.add_node("model", call_model)
-
-  memory = MemorySaver()
-  app = workflow.compile(checkpointer=memory)
-
-  config = {"configurable": {"thread_id": "abc123"}}
+  
+  config = {"configurable": {"thread_id": cid}}
 
   instructions=(
   f"For the following recipe in json... {recipe}, analyze it"
@@ -49,7 +56,6 @@ def create_chat():
   "if the user wants to go back to the recipe, they will say to go back to a instructio number or describe the instruction"
   "then, you will find the step and reexplain the step in a conversational style"
   "after that, you will ask the user if you want to go back to the current step")
-  model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
   
   initial_messages = [
       SystemMessage(content=instructions),
@@ -70,4 +76,32 @@ def create_chat():
     mimetype='audio/mpeg',
     as_attachment=True,
     download_name='intro.mp3'
+  ), 200
+
+
+@chat_bp.route("/chat", methods=["POST"])
+def chat():
+  data = request.get_json()
+  if not data or not all(key in data for key in ['cid', 'userMessage']):
+    return jsonify({'message': 'Missing data: recipe, cid'}), 400
+  cid = data.get('cid')
+  userMessage = data.get('userMessage')
+
+  config = {"configurable": {"thread_id": cid}}
+  
+  response = app.invoke({"messages": [HumanMessage(content=userMessage)]}, config)
+  mp3_fp = io.BytesIO()
+  try:
+    tts = gTTS(response["messages"][-1].content, lang='en')
+    tts.write_to_fp(mp3_fp)
+  except Exception as e:
+    return jsonify({"message":"Speech could not be generated"}), 500
+  
+  mp3_fp.seek(0)
+  
+  return send_file(
+    mp3_fp,
+    mimetype='audio/mpeg',
+    as_attachment=True,
+    download_name='response.mp3'
   ), 200
