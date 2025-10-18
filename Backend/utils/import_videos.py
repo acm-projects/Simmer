ff8b60e1d47827d91f16e0cbb8415f16b3642e41
Hautea <adrian.hautea@gmail.com>
2025-10-18 18:00:29 -0500

    Added caption functionality to import_videos

--- Backend/utils/import_videos.py
+++ Backend/utils/import_videos.py
@@ -15,32 +15,41 @@ def generate_recipe_from_tiktok(video_url):
     try:
 
         encoded_url = urllib.parse.quote(video_url, safe="")
-        api_url = f"https://api.scrapecreators.com/v1/tiktok/video/transcript?url={encoded_url}"
+        api_url = f'https://api.scrapecreators.com/v2/tiktok/video?url={encoded_url}&get_transcript=true'
         headers = {
-            "x-api-key" : SCRAPECREATORS_API_KEY
+            'x-api-key' : SCRAPECREATORS_API_KEY
         }
-        transcript_response = requests.get(api_url, headers=headers)
+        response = requests.get(api_url, headers=headers)
 
-        if transcript_response.status_code != 200:
-            print("Transcript fetch failed:", transcript_response.text)
+        if response.status_code != 200:
+            print('Transcript fetch failed:', response.text)
             return
 
-        transcript_data = transcript_response.json()
-        transcript = transcript_data.get("transcript", "")
+        data = response.json()
+        aweme_detail = data.get('aweme_detail', {})
+        transcript = data.get('transcript', '')
+        caption = aweme_detail.get('desc', '')
+
+        if not caption:
+            print('No caption found.')
 
         if not transcript:
-            print("No transcript found in API Response.")
+            print('No transcript found in API Response.')
             return
         
-        model = genai.GenerativeModel("gemini-2.5-flash")
+        model = genai.GenerativeModel('gemini-2.5-flash')
 
-        prompt = f"""
-        You are a cooking assistant. Analyze this cooking transcript from a TikTok video and turn it into a structure recipe JSON ONLY.
+        prompt = f'''
+        You are a cooking assistant. Analyze this cooking transcript and respective caption from a TikTok video and turn it into a structure recipe JSON ONLY.
         Make reasonable assumptions about prep and cook time if not stated.
-        Infer the ingredients list based on the context and instructions provided in the transcript.
+        Fill out the ingredients in the JSON based on the caption. Make sure the quantity and unit fields match what is in the caption.
+        If caption is empty and/or doesn't list ingredients, infer ingredient list based on context of the transcript.
 
         Transcript:
         {transcript}
+
+        Caption:
+        {caption}
 
         Strictly output *only* the recipe JSON is this format (no markdown, no extra text):
 
@@ -59,25 +68,26 @@ def generate_recipe_from_tiktok(video_url):
             {{"name": "", "quantity": "", "unit": "", "is_allergen": false}}
           ]
         }}
-        """
+        '''
+
         gemini_response = model.generate_content(prompt)
         recipe_text = gemini_response.text.strip()
 
         try:
-            if recipe_text.startswith("```"):
-                recipe_text = recipe_text.strip("` \n").replace("json\n", "")
+            if recipe_text.startswith('```'):
+                recipe_text = recipe_text.strip('` \n').replace('json\n', '')
 
             recipe_json = json.loads(recipe_text)
             print(json.dumps(recipe_json, indent=2))
             return recipe_json
 
         except json.JSONDecodeError:
-            print("Could not parse JSON, showing raw Gemini output instead:\n")
+            print('Could not parse JSON, showing raw Gemini output instead:\n')
             print(recipe_text)
             return recipe_text
 
     except Exception as e:
-        print("Error: ", e)
+        print('Error: ', e)
 
 if __name__ == "__main__":
-    generate_recipe_from_tiktok("https://www.tiktok.com/t/ZTMPY9Yyq/")
+    generate_recipe_from_tiktok("https://www.tiktok.com/t/ZTMaanTGB/")
