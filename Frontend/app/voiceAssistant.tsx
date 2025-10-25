c7de609194cafc4a90fa560959d188083e07979a
root <hieu.q.tran532@gmail.com>
2025-10-25 17:44:01 -0500

    recorder to expo-audio

--- Frontend/app/voiceAssistant.tsx
+++ Frontend/app/voiceAssistant.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Alert, Button, View, TextInput, Text, TouchableOpacity , StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context'
 import { Audio } from 'expo-av';
-import { AudioPlayer, useAudioPlayer, createAudioPlayer,setAudioModeAsync } from 'expo-audio';
+import { AudioPlayer, useAudioPlayer, createAudioPlayer,setAudioModeAsync, AudioRecorder, AudioModule, useAudioRecorder,  RecordingPresets } from 'expo-audio';
 // import * as FileSystem from 'expo-file-system';
 import * as FileSystem from 'expo-file-system/legacy';
 
@@ -36,13 +36,13 @@ const recordingOptions = {
 };
 export default function VoiceAssistant() {
 
-  const [recordingStatus, setRecordingStatus]=useState('start')
+  const [recordingStatus, setRecordingStatus]=useState(false)
   const [recording, setRecording] = useState<Audio.Recording | undefined>();
   const [permissionResponse, requestPermission] = Audio.usePermissions();
   const [recordingUri, setRecordingUri] = useState<string | null>(null);
   const [sound, setSound] = useState<Audio.Sound | undefined>();
   const player: AudioPlayer = useAudioPlayer();
-
+  const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
   useEffect(()=>{
     const setAudioMode= async()=>{
       try {
@@ -62,66 +62,88 @@ export default function VoiceAssistant() {
   },[])
 
 
-  async function startRecording() {
-    if(!permissionResponse){
-      Alert.alert('permissionResponse is null')
-      return;
+  // async function startRecording() {
+  //   if(!permissionResponse){
+  //     Alert.alert('permissionResponse is null')
+  //     return;
+  // }
+  //   try {
+  //     if (permissionResponse.status !== 'granted') {
+  //       console.log('Requesting permission..');
+  //       await requestPermission();
+  //     }
+  //     await Audio.setAudioModeAsync({
+  //       allowsRecordingIOS: true,
+  //       playsInSilentModeIOS: true,
+  //     });
+
+  //     console.log('Starting recording..');
+  //     const { recording } = await Audio.Recording.createAsync( recordingOptions
+  //     );
+  //     setRecording(recording);
+  //     console.log('Recording started');
+  //   } catch (err) {
+  //     console.error('Failed to start recording', err);
+  //   }
+  // }
+  async function startRecording(){
+    const status = await AudioModule.requestRecordingPermissionsAsync();
+    if (!status.granted) {
+      Alert.alert('Permission to access microphone was denied');
+    }
+    await audioRecorder.prepareToRecordAsync();
+    audioRecorder.record();
+    setRecordingStatus(true)
   }
-    try {
-      if (permissionResponse.status !== 'granted') {
-        console.log('Requesting permission..');
-        await requestPermission();
-      }
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      });
 
-      console.log('Starting recording..');
-      const { recording } = await Audio.Recording.createAsync( recordingOptions
-      );
-      setRecording(recording);
-      console.log('Recording started');
-    } catch (err) {
-      console.error('Failed to start recording', err);
-    }
-  }
+  // async function stopRecording() {
+  //   console.log('Stopping recording..');
+  //   if(!recording){
+  //     console.log('recording doesnt exist')
+  //     return
+  //   }
+  //   await recording.stopAndUnloadAsync();
+  //   await Audio.setAudioModeAsync(
+  //     {
+  //       allowsRecordingIOS: false,
+  //     }
+  //   );
+  //   const uri = recording.getURI();
+  //   setRecordingUri(uri);
+  //   console.log('Recording stopped and stored at', uri);
+  //   setRecording(undefined);
+  // }
+  const stopRecording = async () => {
 
-  async function stopRecording() {
-    console.log('Stopping recording..');
-    if(!recording){
-      console.log('recording doesnt exist')
-      return
-    }
-    await recording.stopAndUnloadAsync();
-    await Audio.setAudioModeAsync(
-      {
-        allowsRecordingIOS: false,
-      }
-    );
-    const uri = recording.getURI();
-    setRecordingUri(uri);
-    console.log('Recording stopped and stored at', uri);
-    setRecording(undefined);
-  }
-  async function playBackAudio() {
-    if (!recordingUri) {
-      console.log("No recording URI found.");
-      return;
-    }
+    await audioRecorder.stop();
+    setRecordingUri(audioRecorder.uri);
+    setRecordingStatus(false)
+  };
+
+  // async function playBackAudio() {
+  //   if (!recordingUri) {
+  //     console.log("No recording URI found.");
+  //     return;
+  //   }
 
 
-    try {
-      const { sound } = await Audio.Sound.createAsync(
-        { uri: recordingUri }
-      );
-      setSound(sound);
+  //   try {
+  //     const { sound } = await Audio.Sound.createAsync(
+  //       { uri: recordingUri }
+  //     );
+  //     setSound(sound);
 
-      await sound.playAsync();
+  //     await sound.playAsync();
 
-    } catch (error) {
-        console.error("Failed to play sound", error);
-    }
+  //   } catch (error) {
+  //       console.error("Failed to play sound", error);
+  //   }
+  // }
+  async function playBackAudio(){
+    let newValue: string | undefined = recordingUri === null ? undefined : recordingUri;
+    const player = createAudioPlayer({uri:newValue});
+
+    player.play();
   }
 
   async function upload() {
@@ -216,7 +238,7 @@ export default function VoiceAssistant() {
 
   return (
     <SafeAreaView >
-      <Button title={recordingStatus} onPress={recording ? stopRecording : startRecording}/>
+      <Button title={'play'} onPress={recordingStatus ? stopRecording : startRecording}/>
       <Button title="play back audio" onPress={playBackAudio}/>
       <Button title="upload" onPress={upload}/>
       
