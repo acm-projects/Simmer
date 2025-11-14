4b9036917bd29859a04ca4c31d2b58e0aa279cb5
Patrick Enerio <penerio2004@gmail.com>
2025-11-13 20:06:48 -0600

    Tweaks to app.py and requirements.txt to fix pyaudio errors

--- Frontend/app/(auth)/login.tsx
+++ Frontend/app/(auth)/login.tsx
@@ -2,10 +2,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Link, useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import { Alert, Button, View, TextInput, Text, KeyboardAvoidingView, Platform, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { Alert, Button, View, TextInput, Text, KeyboardAvoidingView, Platform, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import { useSupabase } from '../contexts/SupabaseContext';
 import {SafeAreaView} from 'react-native-safe-area-context'
 import {InputField} from '@/components/inputField'
+import {Image} from 'expo-image';
 
 
 export default function Login() {
